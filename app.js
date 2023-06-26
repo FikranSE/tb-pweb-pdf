fe8708b72195f8f6ab0d543ea2a740cf4a1c6a56
fikranshadiq <fikranelyafit@gmail.com>
2023-06-26 17:37:07 +0700

    add vercel.json

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.use(flash());
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
